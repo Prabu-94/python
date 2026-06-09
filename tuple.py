df37384da98e64122583069b96c12ff85527137d
Prabu-94 <prabuspu@gmail.com>
2026-06-09 17:44:35 +0530

    tuples practice

---
+++ tuple.py
@@ -0,0 +1,27 @@
+# Create a tuple and print its length.
+# Find the maximum and minimum values in a tuple.
+# Convert a tuple into a list.
+# Count occurrences of an element in a tuple.
+# Check whether an element exists in a tuple.
+
+#question 1:
+a=(1,2,3,4,5)
+print(len(a))
+
+#question 2:
+a=(1,2,3,4,5)
+print(max(a))
+print(min(a))
+
+#question 3:
+a=(1,2,3,4,5)
+b=list(a)
+print(b)
+
+#question 4:
+a=(1,2,3,4,5,2,2)
+print(a.count(2))
+
+#question 5:
+a=(1,2,3,4,5)
+print(3 in a)
